44f72b18ec967359125bdca6592f632af70d6eb8
Ku4eR <Ku4eR99@gmail.com>
2021-10-19 01:41:09 +0300

    ДЗ №2. Перезаливаю всю сборку, так как были небольшие неполадки. Переименованы некоторые компоненты решения и дирректории. Решение было разделено на библиотеку(.DLL) с приватным классом, которая содержит в себе приватный статический метод для подсчета слов в тексте. Второй частью решения является .EXE файл, который загружает данные с файла и пишет слова и их кол-во. Также, здесь используется REFLECTION для получения доступа к приватному методу класса Counter  в namespace WordCounter.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string AllText = File.ReadAllText("Warlockshadow.txt");
+            string Write = @"Words.txt";
+            //-------REFLECTION----------//
+
+            var types = Assembly.LoadFrom("WordCounter.dll").GetTypes();
+            var type = types.FirstOrDefault(x => x.Name == "Counter");
+            var exemp = Activator.CreateInstance(type);
+            var method = exemp.GetType().GetMethod("GoCount", System.Reflection.BindingFlags.NonPublic | BindingFlags.Static);
+            Dictionary<string, int> rslt = (Dictionary<string,int>)method.Invoke(null, new object[] {AllText});
+
+            //--------------------------//
+           
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(Write, false, System.Text.Encoding.Default))
+                {
+                    foreach (var word in rslt) {
+                        await sw.WriteLineAsync($"Word: {word.Key}. Count: {word.Value}");
+                    }
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+        }
+    }
+}
---
+++ WordCounter/Counter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCounter
+{
+    class Counter
+    {
+        private static Dictionary<string, int> GoCount(string AllText)
+        {
+            var AllWords = AllText.Split(' ', '-', ':', '.', '"', '\'', '!', '?').Where(q => !string.IsNullOrEmpty(q));
+            var Uniqs = AllWords.Select(q => q.ToLower().Trim()).Distinct();
+            var rslt = new Dictionary<string, int>();
+            foreach (var word in Uniqs)
+            {
+                rslt.Add(word, AllWords.Count(q => q.ToLower().Equals(word)));
+            }
+            rslt = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
+            return rslt;
+        }
+    }
+}
