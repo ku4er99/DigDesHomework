8ea27d9c4aaeb35f80ad901fe6a481f746ac4659
Ku4eR <Ku4eR99@gmail.com>
2021-10-22 21:11:05 +0300

    ДЗ№ 3. В .DLL библиотеку добавлен публичный метод, аналогичный приватному. Метод использует для подсчета уникальных слов потокобезопастный тип данных ConcurrentDictionary<TKey,TValue>. Цикл foreach распараллелен (заменен) на аналог - Parallel.ForEach(args). Немного изменены переменные, приложение переведено на английский язык. Зачем? Непонятно.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -4,26 +4,55 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Reflection;
-
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static string AllText = File.ReadAllText("Warlockshadow.txt");
+        static string Write = @"Words.txt";
+        static IEnumerable<string> AllWords = AllText.Split(' ', '-', ':', '.', '"', '\'', '!', '?').Where(q => !string.IsNullOrEmpty(q));
+        static IEnumerable<string> Uniqs = AllWords.Select(q => q.ToLower().Trim()).Distinct();
+        public static Dictionary<string, int> ReflectionMethod()
         {
-            string AllText = File.ReadAllText("Warlockshadow.txt");
-            string Write = @"Words.txt";
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             //-------REFLECTION----------//
 
             var types = Assembly.LoadFrom("WordCounter.dll").GetTypes();
             var type = types.FirstOrDefault(x => x.Name == "Counter");
             var exemp = Activator.CreateInstance(type);
             var method = exemp.GetType().GetMethod("GoCount", System.Reflection.BindingFlags.NonPublic | BindingFlags.Static);
-            Dictionary<string, int> rslt = (Dictionary<string,int>)method.Invoke(null, new object[] {AllText});
+            Dictionary<string, int> rslt = (Dictionary<string, int>)method.Invoke(null, new object[] { AllText, AllWords, Uniqs });
 
             //--------------------------//
-           
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("RunTime with Reflection: " + time);
+            return rslt;
+        }
+        public static Dictionary<string, int> ParallelMethod()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Dictionary<string, int> rslt = WordCounter.Counter.GoCountParallel(AllText, AllWords, Uniqs);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("RunTime with ConcurrentDictionary & Parallel.ForEach: " + time);
+            return rslt;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Counting the program execution time in the format:");
+            Console.WriteLine("[Hours]:[Minutes]:[Seconds].[Milliseconds]");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            ReflectionMethod();
+            var rslt = ParallelMethod();
+
             try
             {
                 using (StreamWriter sw = new StreamWriter(Write, false, System.Text.Encoding.Default))
@@ -32,12 +61,14 @@ namespace ConsoleApp1
                         await sw.WriteLineAsync($"Word: {word.Key}. Count: {word.Value}");
                     }
                 }
-                Console.WriteLine("Запись выполнена");
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine("\t\tRecording completed!");
+                Console.WriteLine("-------------------------------------------------------------------------------");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-            } 
+            }
         }
     }
 }
--- WordCounter/Counter.cs
+++ WordCounter/Counter.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace WordCounter
 {
-    class Counter
+    public class Counter
     {
-        private static Dictionary<string, int> GoCount(string AllText)
+        private static Dictionary<string, int> GoCount(string AllText, IEnumerable<string> AllWords, IEnumerable<string> Uniqs)
         {
-            var AllWords = AllText.Split(' ', '-', ':', '.', '"', '\'', '!', '?').Where(q => !string.IsNullOrEmpty(q));
-            var Uniqs = AllWords.Select(q => q.ToLower().Trim()).Distinct();
             var rslt = new Dictionary<string, int>();
             foreach (var word in Uniqs)
             {
@@ -16,6 +16,17 @@ namespace WordCounter
             }
             rslt = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
             return rslt;
+        }
+
+        public static Dictionary<string, int> GoCountParallel(string AllText, IEnumerable<string> AllWords, IEnumerable<string> Uniqs)
+        {
+            var rslt = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(Uniqs, word =>
+            {
+                rslt.TryAdd(word, AllWords.Count(q => q.ToLower().Equals(word)));
+            });
+            var rslt2 = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
+            return rslt2;
         }
     }
 }
