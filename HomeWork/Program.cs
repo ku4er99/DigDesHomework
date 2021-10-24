8a8bc1983d24a1713aa30e65f0cda569e07be66a
Ku4eR <Ku4eR99@gmail.com>
2021-10-24 22:09:07 +0300

    Промежуточный комит для подтягивания производительности. Метод подсчета слов изменен. Для наглядности создан большой файл (>45mb) BIG.txt, с которого производится подсчет. Многопоточность работает корректно.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -10,37 +10,38 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static string AllText = File.ReadAllText("Warlockshadow.txt");
+        static string AllText = File.ReadAllText("BIG.txt").ToLower();
         static string Write = @"Words.txt";
-        static IEnumerable<string> AllWords = AllText.Split(' ', '-', ':', '.', '"', '\'', '!', '?').Where(q => !string.IsNullOrEmpty(q));
-        static IEnumerable<string> Uniqs = AllWords.Select(q => q.ToLower().Trim()).Distinct();
+        static string[] textArray = AllText.Split(new char[] { ' ', '-', ':', '.', '"', '\'', '!', '?', '\n', '[', ']', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
         public static Dictionary<string, int> ReflectionMethod()
         {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            //-------REFLECTION----------//
 
+            //-------REFLECTION----------//
             var types = Assembly.LoadFrom("WordCounter.dll").GetTypes();
             var type = types.FirstOrDefault(x => x.Name == "Counter");
             var exemp = Activator.CreateInstance(type);
             var method = exemp.GetType().GetMethod("GoCount", System.Reflection.BindingFlags.NonPublic | BindingFlags.Static);
-            Dictionary<string, int> rslt = (Dictionary<string, int>)method.Invoke(null, new object[] { AllText, AllWords, Uniqs });
-
+            Dictionary<string, int> rslt = (Dictionary<string, int>)method.Invoke(null, new object[] { textArray });
             //--------------------------//
+
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
-            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
             Console.WriteLine("RunTime with Reflection: " + time);
             return rslt;
         }
+
         public static Dictionary<string, int> ParallelMethod()
         {
+            Dictionary<string, int> rslt = new Dictionary<string, int>();
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            Dictionary<string, int> rslt = WordCounter.Counter.GoCountParallel(AllText, AllWords, Uniqs);
+            rslt = WordCounter.Counter.GoCountParallel(textArray);
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
-            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            string time = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
             Console.WriteLine("RunTime with ConcurrentDictionary & Parallel.ForEach: " + time);
             return rslt;
         }
--- WordCounter/Counter.cs
+++ WordCounter/Counter.cs
@@ -7,25 +7,24 @@ namespace WordCounter
 {
     public class Counter
     {
-        private static Dictionary<string, int> GoCount(string AllText, IEnumerable<string> AllWords, IEnumerable<string> Uniqs)
+        private static Dictionary<string, int> GoCount(string[] textArray)
         {
-            var rslt = new Dictionary<string, int>();
-            foreach (var word in Uniqs)
-            {
-                rslt.Add(word, AllWords.Count(q => q.ToLower().Equals(word)));
-            }
-            rslt = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
-            return rslt;
+            var result = textArray.GroupBy(x => x).Where(x => x.Count() > 1)
+                .ToDictionary(key => key.Key, value => value.Count())
+                .OrderByDescending(q => q.Value).ToDictionary(key => key.Key, value => value.Value); 
+            return result;
         }
 
-        public static Dictionary<string, int> GoCountParallel(string AllText, IEnumerable<string> AllWords, IEnumerable<string> Uniqs)
+
+        public static Dictionary<string, int> GoCountParallel(string[] textArray)
         {
             var rslt = new ConcurrentDictionary<string, int>();
-            Parallel.ForEach(Uniqs, word =>
+            var rslt2 = new Dictionary<string, int>();
+            Parallel.ForEach(textArray, word =>
             {
-                rslt.TryAdd(word, AllWords.Count(q => q.ToLower().Equals(word)));
+                rslt.AddOrUpdate(word, 1, (Word, oldValue) => oldValue+1);
             });
-            var rslt2 = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
+            rslt2 = rslt.OrderByDescending(q => q.Value).ToList().ToDictionary(key => key.Key, value => value.Value);
             return rslt2;
         }
     }
